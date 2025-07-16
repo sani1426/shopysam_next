a94642209e1e2010890601aaf1c7c12d9c6ff7f4
saman <sam.karimi727@gmail.com>
2025-07-17 02:03:21 +0330

    build(deps): update layout.jsx, package-lock.json and package.json

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <AppContextProvider>
             <main>{children}</main>
+            <Toaster position="top-center" richColors />
           </AppContextProvider>
         </Providers>
       </body>

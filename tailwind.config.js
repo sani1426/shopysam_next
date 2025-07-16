51dd416dc2f2704bf7543fc0d9e664f21f7f6817
saman <sam.karimi727@gmail.com>
2025-07-17 01:17:40 +0330

    build(deps): update package-lock.json, package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},

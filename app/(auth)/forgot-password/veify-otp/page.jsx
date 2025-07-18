65dc83efc13515173008ede9ba2c075219244306
saman <sam.karimi727@gmail.com>
2025-07-19 00:02:04 +0330

    move 1 file and update 1 file

--- app/(auth)/forgot-password/veify-otp/page.jsx
+++ app/(auth)/forgot-password/veify-otp/page.jsx
@@ -3,9 +3,9 @@
 import { Suspense } from 'react'
 import VerifyOtp from '@/components/auth/VerifyOtpSuspense'
 
-// function SuspenseFallback() {
-//   return <>placeholder</>
-// }
+function SuspenseFallback() {
+  return <>placeholder</>
+}
 
 export default async function page({searchParams}) {
  

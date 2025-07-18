e6b76a6a2e794f6c7cc0c60ca8251c9bf0a46e19
saman <sam.karimi727@gmail.com>
2025-07-19 00:42:54 +0330

    update 2 files, create 2 files and delete 1 file

--- app/(auth)/forgot-password/veify-otp/page.jsx
+++ app/(auth)/forgot-password/veify-otp/page.jsx
@@ -1,23 +1,35 @@
-
 import { Spinner } from '@heroui/react'
 import { Suspense } from 'react'
-import VerifyOtp from '@/components/auth/VerifyOtpSuspense'
+import VerifyOtp from '@/components/auth/VerifyOtpComponent'
 
 function SuspenseFallback() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <Spinner color='success' label='Loading...' labelColor='success' variant='dots' size='lg' />
+      <Spinner
+        color='success'
+        label='Loading...'
+        labelColor='success'
+        variant='dots'
+        size='lg'
+      />
     </div>
   )
 }
 
-export default  function page() {
- 
-
-
+export default function page(props) {
   return (
-    <Suspense fallback={<SuspenseFallback />}>
-      <VerifyOtp  />
-       </Suspense>
+    <div className='w-full h-[100vh] flex flex-col justify-center items-center'>
+      <div
+        style={{ width: '35rem', maxWidth: '95%' }}
+        className='rounded-xl py-5 bg_back-1 shadow-xl '
+      >
+        <h1 className='mb-10 text-center text-5xl bg-gradient-to-r from-blue-600  via-red-600 to-orange-600 bg-clip-text text-transparent'>
+          Verify Otp
+        </h1>
+        <Suspense fallback={<SuspenseFallback />}>
+          <VerifyOtp gmail={props} />
+        </Suspense>
+      </div>
+    </div>
   )
 }
---
+++ components/auth/VerifyOtpComponent.jsx
@@ -0,0 +1,67 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import SummaryApi from '@/common'
+import { InputOtp } from '@heroui/react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import axios from 'axios'
+
+export default function VerifyOtp({ gmail }) {
+  const searchParams = useSearchParams()
+  const Email = searchParams.get('email')
+  const [send, setSend] = useState(false)
+  const router = useRouter()
+  const [otp, setOtp] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setSend(true)
+    const { data } = await axios.put(
+      SummaryApi?.verifyOtp?.url,
+      {
+        email: Email,
+        otp: otp,
+      },
+      { withCredentials: true }
+    )
+    setSend(false)
+    if (data.success) {
+      toast.success(data?.message)
+      setTimeout(() => {
+        router.push(`forgot-password/reset-password?email=${Email}`)
+      }, 1000)
+    }
+    if (data.error) {
+      toast.error(data.message)
+    }
+  }
+  useEffect(() => {
+    console.log('gmail', gmail)
+  }, [])
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='flex flex-col gap-5 justify-center w-full items-center'
+    >
+      <div className='w-full flex flex-col gap-2 justify-center items-center'>
+        <InputOtp
+          variant='bordered'
+          size='lg'
+          length={6}
+          value={otp}
+          onValueChange={setOtp}
+        />
+        <p className='text-default-500 text-small'>
+          enter otp code was sent to your email
+        </p>
+      </div>
+      <button
+        className={`w-[80%] bg-blue-600 py-2 rounded-xl hover:bg-blue-800 transition-all duration-300 text-white cursor-pointer ${
+          send ? 'opacity-60' : ''
+        }`}
+      >
+        {send ? 'Sending ...' : 'Send'}
+      </button>
+    </form>
+  )
+}

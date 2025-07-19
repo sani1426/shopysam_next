fd13a5c833eec91cd8a2149a88e5e36cfc4ee50f
saman <sam.karimi727@gmail.com>
2025-07-20 01:22:18 +0330

    update page.jsx

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -3,8 +3,9 @@ import HeroCarousel from "@/components/main/HeroCarousel";
 
 export default function Home() {
   return (
-<div  className='mt-[10.8rem] lg:mt-[12.2rem]'>
+<>
 <HeroCarousel />
-</div>
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+</>
   );
 }

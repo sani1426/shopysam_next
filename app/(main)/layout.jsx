83da3a684822154cfb2c5f9fcc1241c58c3e345c
saman <sam.karimi727@gmail.com>
2025-07-20 01:20:44 +0330

    update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -2,10 +2,14 @@ import Header from '@/components/shared/Header'
 
 const layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      <div>{children}</div>
-    </div>
+    <div className='flex flex-col overflow-hidden'>
+    <Header  />
+ 
+       <main className='flex flex-col w-full mt-[10rem] lg:mt-[12.5rem]  '>
+         {children}
+       </main>
+
+     </div>
   )
 }
 

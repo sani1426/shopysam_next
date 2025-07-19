4a17c52b84a587d74ced20ec5c205762aae30dfc
saman <sam.karimi727@gmail.com>
2025-07-20 01:05:29 +0330

    update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,13 +1,12 @@
-import Header from "@/components/shared/Header"
-
+import Header from '@/components/shared/Header'
 
 const layout = ({ children }) => {
-        return (
-          <main>
-            <Header />
-              {children}
-          </main>
-        )
-      }
-      
-      export default layout
+  return (
+    <div>
+      <Header />
+      <div className='mt-[10.8rem] lg:mt-[12.2rem]'>{children}</div>
+    </div>
+  )
+}
+
+export default layout

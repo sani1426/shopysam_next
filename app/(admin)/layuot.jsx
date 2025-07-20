115f7dfed444f0ebe68b5c0cf29f665d94fc241b
saman <sam.karimi727@gmail.com>
2025-07-20 23:42:23 +0330

    update layuot.jsx

--- app/(admin)/layuot.jsx
+++ app/(admin)/layuot.jsx
@@ -1,24 +1,16 @@
-import AdminHeader from "@/components/admin/adminHeader"
-import AdminSidebar from "@/components/admin/adminSidebar"
-
+import AdminHeader from '@/components/admin/adminHeader'
+import AdminSidebar from '@/components/admin/adminSidebar'
 
 const layout = ({ children }) => {
-        return (
-          <section>
-            <AdminHeader />
-            <div className="container mx-auto p-3">
-              <div>
-                <div>
-                <AdminSidebar />
-                </div>
-                <div>
-                {children}
-                </div>
-              </div>
-            </div>
-              
-          </section>
-        )
-      }
-      
-      export default layout
+  return (
+    <section>
+      <AdminHeader />
+
+      <AdminSidebar />
+
+      {children}
+    </section>
+  )
+}
+
+export default layout

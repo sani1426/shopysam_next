16da3bde4ef58d952c76ad7be81fb0f0534a3806
saman <sam.karimi727@gmail.com>
2025-07-20 23:52:55 +0330

    create 1 file and delete 1 file

---
+++ app/(admin)/layout.jsx
@@ -0,0 +1,15 @@
+import AdminHeader from "@/components/admin/adminHeader"
+import AdminSidebar from "@/components/admin/adminSidebar"
+
+
+const layout = ({ children }) => {
+  return (
+    <main>
+      <AdminHeader />
+      <AdminSidebar />
+        {children}
+    </main>
+  )
+}
+
+export default layout

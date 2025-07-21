d085962f7759651e2f6973474e2220d55482eb81
saman <sam.karimi727@gmail.com>
2025-07-21 22:55:35 +0330

    update adminSidebar.jsx

--- components/admin/adminSidebar.jsx
+++ components/admin/adminSidebar.jsx
@@ -12,7 +12,7 @@ const AdminSidebar = () => {
     <div className='fixed left-0 w-[250px] h-full shadow-xl '>
        <div className='font-semibold'>My Account</div>
         <div className='text-sm flex items-center gap-2'>
-          <span className='max-w-52 text-ellipsis line-clamp-1'>{user.name || user.mobile} <span className='text-medium text-red-600'>{user.role === "ADMIN" ? "(Admin)" : "" }</span></span>
+          <span className='max-w-52 text-ellipsis line-clamp-1'>{userDetail?.name || userDetail?.mobile} <span className='text-medium text-red-600'>{userDetail?.role === "ADMIN" ? "(Admin)" : "" }</span></span>
           <Link  href={"/dashboard/profile"} className='hover:text-primary-200'>
             <HiOutlineExternalLink size={15}/>
           </Link>

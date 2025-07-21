ad045f390806fd05eae9c2944335a81fb154674e
saman <sam.karimi727@gmail.com>
2025-07-22 01:16:53 +0330

    update page.jsx

--- app/(admin)/admin/dashboard/page.jsx
+++ app/(admin)/admin/dashboard/page.jsx
@@ -1,14 +1,15 @@
 "use client"
 import EditAvatarModal from '@/components/admin/EditAvatarModal'
 import { useAppContext } from '@/context/appContext'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaRegUserCircle } from 'react-icons/fa'
 
 const page = () => {
   const { userDetail, setUserDetail } = useAppContext()
+  useEffect(()=>{console.log(userDetail)},[])
   return (
-  <div>
-     <div className='w-20 h-20 bg-red-500 flex items-center justify-center rounded-full overflow-hidden drop-shadow-sm'>
+  <div className='p-8'>
+     <div className='w-20 h-20  flex items-center justify-center rounded-full overflow-hidden drop-shadow-sm'>
             {
                 userDetail?.avatar ? (
                     <img 

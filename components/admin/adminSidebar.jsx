8d083c4b598e627d66977e0cb631ff8559d45127
saman <sam.karimi727@gmail.com>
2025-07-21 23:03:52 +0330

    update page.jsx and adminSidebar.jsx

--- components/admin/adminSidebar.jsx
+++ components/admin/adminSidebar.jsx
@@ -9,15 +9,21 @@ const AdminSidebar = () => {
   const { userDetail, setUserDetail } = useAppContext()
 
   return (
-    <div className='fixed left-0 w-[250px] h-full shadow-xl '>
+    <div className='fixed left-0 w-[300px] h-full shadow-xl '>
        <div className='font-semibold'>My Account</div>
         <div className='text-sm flex items-center gap-2'>
-          <span className='max-w-52 text-ellipsis line-clamp-1'>{userDetail?.name || userDetail?.mobile} <span className='text-medium text-red-600'>{userDetail?.role === "ADMIN" ? "(Admin)" : "" }</span></span>
+          <span className='max-w-52 text-ellipsis line-clamp-1'>{userDetail?.name || userDetail?.mobile} <span className='text-medium text-red-600'>{userDetail?.role === "ADMIN" ? "(Admin)" : "(user)" }</span></span>
           <Link  href={"/dashboard/profile"} className='hover:text-primary-200'>
             <HiOutlineExternalLink size={15}/>
           </Link>
         </div>
         <Divider className='my-4' />
+
+        <ul className='flex flex-col gap-2 w-full'>
+          <li><Link>My Orders</Link></li>
+          <li><Link>Save Address</Link></li>
+          <li><Link>Logout</Link></li>
+        </ul>
     </div>
   )
 }

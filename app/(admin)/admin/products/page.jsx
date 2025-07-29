6443a670fc5d031efeca9afa3dd725c18d5f595e
saman <sam.karimi727@gmail.com>
2025-07-30 02:43:25 +0330

    update page.jsx

--- app/(admin)/admin/products/page.jsx
+++ app/(admin)/admin/products/page.jsx
@@ -3,6 +3,7 @@ import AdminNav from '@/components/admin/adminNav'
 import { useAppContext } from '@/context/appContext'
 import '@/components/admin/admin.css'
 import AllProducts from '@/components/admin/product/allProduct'
+import { useState } from 'react'
 
 
 const page = () => {

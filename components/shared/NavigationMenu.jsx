846d2ad71298535ed2e16d0c58496f2d20b4f4ea
saman <sam.karimi727@gmail.com>
2025-08-02 00:33:15 +0330

    update NavigationMenu.jsx

--- components/shared/NavigationMenu.jsx
+++ components/shared/NavigationMenu.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const NavigationMenu = () => {
   return (
-    <div className='w-full flex items-center gap-16 nav-clr   py-2   mt-4 absolute top-[78px] md:top-[85px]  lg:top-[116px] px-3  lg:px-12  border-slate-300 z-40 shadow-md  dark:shadow-gray-900'>
+    <div className='w-full flex items-center gap-16 nav-clr   py-2   mt-4 absolute top-[85px] md:top-[92px]  lg:top-[124px] px-3  lg:px-12  border-slate-300 z-40 shadow-md  dark:shadow-gray-900'>
       <CategoryMenu />
 
       <ul className=' items-center gap-10 hidden lg:flex dark:text-gray-300'>
@@ -19,7 +19,7 @@ const NavigationMenu = () => {
 
       <h1 className=' items-center gap-1 ml-32 hidden xl:flex text-gray-600'>
         <span className='text-3xl'><IoRocketOutline /></span>
-        <span className="text-sm">Free International Delivery</span>
+        <span className="text-xs text-nowrap">Free International Delivery</span>
         </h1>
     </div>
   )

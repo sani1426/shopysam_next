9c6d1d34b5389e594836216daa52b66950edc53f
saman <sam.karimi727@gmail.com>
2025-08-03 01:32:59 +0330

    update ViewImage.jsx

--- components/UI/ViewImage.jsx
+++ components/UI/ViewImage.jsx
@@ -1,20 +1,18 @@
-import Image from 'next/image'
+
 import React from 'react'
 import { IoClose } from 'react-icons/io5'
 
 const ViewImage = ({url,close}) => {
   return (
     <div className='fixed w-full max-h-screen top-0 bottom-0 right-0 left-0 bg-neutral-900 bg-opacity-70 flex justify-center items-center z-50 p-4'>
-        <div className='relative w-full max-w-md max-h-[80vh] p-4 bg-white'>
+        <div className='max-w-md max-h-[80vh] p-4 bg-white'>
             <button onClick={close} className='w-fit ml-auto block'>
                 <IoClose size={25}/>
             </button>
-            <Image
+            <img
                 src={url}
                 alt='full screen'
-                fill
-                priority
-                className=' object-scale-down'
+                className='w-full h-full object-scale-down'
             />
         </div>
     </div>

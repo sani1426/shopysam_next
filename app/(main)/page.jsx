f84a3d700ccee9d72fd11fe9e0104f7e32ed7d51
saman <sam.karimi727@gmail.com>
2025-08-05 00:36:18 +0330

    update 1 file and create 1 file

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,12 +1,12 @@
+import CategorySlider from "@/components/main/CategorySlider";
 import HeroCarousel from "@/components/main/HeroCarousel";
-import ShowCategory from "@/components/main/ShowCategory";
 
 
 export default function Home() {
   return (
 <>
 <HeroCarousel />
-    <ShowCategory />
+    <CategorySlider />
 </>
   );
 }

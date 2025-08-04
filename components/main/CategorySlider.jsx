943be559663ca5d0b860be0066c89d37bddb2b14
saman <sam.karimi727@gmail.com>
2025-08-05 00:48:32 +0330

    update CategorySlider.jsx

--- components/main/CategorySlider.jsx
+++ components/main/CategorySlider.jsx
@@ -1,39 +1,39 @@
-"use client"
+'use client'
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/grid';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/grid'
+import 'swiper/css/pagination'
 
-import { Grid, Pagination } from 'swiper/modules';
-import Axios from '@/utils/axios';
+import { Grid, Pagination } from 'swiper/modules'
+import Axios from '@/utils/axios'
 import BackendApi from '@/common/api'
-import AxiosToastError from '@/utils/axiosToastError';
+import AxiosToastError from '@/utils/axiosToastError'
 
 export default function CategorySlider() {
-        const [allCat , setAllCat]=useState([])
-        const [loading , setLoading]=useState(false)
-        const fetchCategory = async () => {
-                try {
-                        setLoading(true)
-                  const response = await Axios({
-                    ...BackendApi.get_Categories,
-                  })
-                  const { data: responseData } = response
-            
-                  if (responseData?.success) {
-                        setAllCat(responseData.data)
-                        setLoading(false)
-                  }
-                } catch (error) {
-                  AxiosToastError(error)
-                } 
-              }
-              useEffect(()=>{
-                fetchCategory()
-              },[])
+  const [allCat, setAllCat] = useState([])
+  const [loading, setLoading] = useState(false)
+  const fetchCategory = async () => {
+    try {
+      setLoading(true)
+      const response = await Axios({
+        ...BackendApi.get_Categories,
+      })
+      const { data: responseData } = response
+
+      if (responseData?.success) {
+        setAllCat(responseData.data)
+        setLoading(false)
+      }
+    } catch (error) {
+      AxiosToastError(error)
+    }
+  }
+  useEffect(() => {
+    fetchCategory()
+  }, [])
   return (
     <>
       <Swiper
@@ -46,35 +46,16 @@ export default function CategorySlider() {
           clickable: true,
         }}
         modules={[Grid, Pagination]}
-        className="mySwiper"
+        className='mySwiper'
       >
         {
-                !loading ? (
-                        allCat.map((_,index) => (
-                                <SwiperSlide key={index}>
-                                        <img src={_?.image} alt={_?.name} />
-                                </SwiperSlide>
-                        ))
-
-                ): (
-                        new Array(12).fill(null).map((c, index) => {
-                                return (
-
-                             <SwiperSlide key={index}>
-                                         <div
-           
-                className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'
-              >
-                <div className='bg-blue-100 min-h-24 rounded'></div>
-                <div className='bg-blue-100 h-8 rounded'></div>
-              </div>
-                                </SwiperSlide>
-                                )
-                              })
-                )
-        }
-    
+         allCat.map((_, index) => (
+              <SwiperSlide className='w-[50px] h-[50px]' key={index}>
+                <img className='max-w-full min-h-full' src={_?.image} alt={_?.name} />
+              </SwiperSlide>
+            ))
+         }
       </Swiper>
     </>
-  );
+  )
 }

99d5e93815cd3e24b603ca9d0a0d435abfdb0dd9
saman <sam.karimi727@gmail.com>
2025-08-04 23:55:52 +0330

    update ShowCategory.jsx

--- components/main/ShowCategory.jsx
+++ components/main/ShowCategory.jsx
@@ -1,42 +1,40 @@
-"use client"
+'use client'
 
-import { useAppContext } from "@/context/appContext"
-import Image from "next/image"
+import { useAppContext } from '@/context/appContext'
+import Image from 'next/image'
 
 const ShowCategory = () => {
-        const {allCategory}= useAppContext()
+  const { allCategory } = useAppContext()
   return (
-        <div className='container mx-auto px-4 my-2 grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10  gap-2'>
-        {
-                allCategory ? (
-                        allCategory?.map((cat,index)=>{
-                                return(
-                                  <div key={cat?._id+"displayCategory"} className='w-full h-full ' >
-                                    <div>
-                                        <Image
-                                          src={cat.image}
-                                          className='w-full h-full object-scale-down'
-                                         width={70}
-                                         height={70}
-                                          loading="lazy"
-                                          placeholder="blur"
-                                        />
-                                    </div>
-                                  </div>
-                                )
-                              })
-                ):(
-                        new Array(12).fill(null).map((c,index)=>{
-                                return(
-                                  <div key={index+"loadingcategory"} className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'>
-                                    <div className='bg-blue-100 min-h-24 rounded'></div>
-                                    <div className='bg-blue-100 h-8 rounded'></div>
-                                  </div>
-                                          )
-                                        })
-                )
-             
-        }
+    <div className='container mx-auto px-4 my-2 grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10  gap-2'>
+      {allCategory
+        ? allCategory?.map((cat, index) => {
+            return (
+              <div
+                key={cat._id + 'displayCategory'}
+                className='w-full h-full'
+                // onClick={() => handleRedirectProductListpage(cat._id, cat.name)}
+              >
+                <div>
+                  <img
+                    src={cat.image}
+                    className='w-full h-full object-scale-down'
+                  />
+                </div>
+              </div>
+            )
+          })
+        : new Array(12).fill(null).map((c, index) => {
+            return (
+              <div
+                key={index + 'loadingcategory'}
+                className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'
+              >
+                <div className='bg-blue-100 min-h-24 rounded'></div>
+                <div className='bg-blue-100 h-8 rounded'></div>
+              </div>
+            )
+          })}
     </div>
   )
 }

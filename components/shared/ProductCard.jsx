ee265c62aefd0bed53fb41c7756743829ccee8ea
saman <sam.karimi727@gmail.com>
2025-08-07 02:06:21 +0330

    update ProductCard.jsx

--- components/shared/ProductCard.jsx
+++ components/shared/ProductCard.jsx
@@ -12,14 +12,14 @@ const CardProduct = ({data}) => {
     const [loading,setLoading] = useState(false)
   
   return (
-    <Link href={`/products/${data?._id}?name=${data?.name}`} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded-tr-xl rounded-bl-xl cursor-pointer bg-white dark:bg-gray-700 dark:text-gray-100 dark:border-gray-100' >
-      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded-t-xl overflow-hidden'>
+    <Link href={`/products/${data?._id}?name=${data?.name}`} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded-tr-xl rounded-bl-xl cursor-pointer  dark:bg-gray-700 dark:text-gray-100 dark:border-gray-100' >
+      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded-tr-xl rounded-bl-xl overflow-hidden'>
             <img 
                 src={data?.image[0]}
                 className='w-full h-full object-scale-down lg:scale-125 rounded-tr-xl rounded-bl-xl'
             />
       </div>
-      <div className='flex items-center gap-1'>
+      <div className='flex items-center justify-between'>
         <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50'>
               10 min 
         </div>
@@ -34,17 +34,23 @@ const CardProduct = ({data}) => {
       <div className='px-2 lg:px-0 font-medium text-ellipsis text-sm lg:text-base line-clamp-2'>
         {data?.name}
       </div>
-      <div className='w-fit gap-1 px-2 lg:px-0 text-sm lg:text-base'>
-        {data?.unit} 
-        
+      <div className='w-full flex items-center justify-between'>
+      <div className='underline text-sm text-red-600'>
+             {data?.price}$
+          </div>
+      <div className='font-semibold text-blue-500'>
+              {pricewithDiscount(data?.price,data?.discount)} $
+          </div>
       </div>
+    
 
       <div className='px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base'>
         <div className='flex items-center gap-1'>
-          <div className='font-semibold'>
-              {pricewithDiscount(data?.price,data?.discount)} $
-          </div>
-          
+     
+        <div className='font-semibold'>
+        {data?.unit} 
+        
+      </div>
           
         </div>
         <div className=''>

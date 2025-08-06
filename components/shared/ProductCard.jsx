6d5e71908922510f1b00d400e3186f916ff192ba
saman <sam.karimi727@gmail.com>
2025-08-07 01:34:48 +0330

    create 2 files and update 1 file

--- components/shared/ProductCard.jsx
+++ components/shared/ProductCard.jsx
@@ -1,7 +1,9 @@
 "use client"
 import { pricewithDiscount } from '@/utils/priceWithDiscount'
+import { Button } from '@heroui/react'
 import Link from 'next/link'
 import { useState } from 'react'
+import { IoCart } from "react-icons/io5";
 
 
 
@@ -10,11 +12,11 @@ const CardProduct = ({data}) => {
     const [loading,setLoading] = useState(false)
   
   return (
-    <Link href="/" className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white' >
-      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden'>
+    <Link href={`/products/${data?._id}`} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded-xl cursor-pointer bg-white dark:bg-gray-700 dark:text-gray-100 dark:border-gray-100' >
+      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded-t-xl overflow-hidden'>
             <img 
                 src={data?.image[0]}
-                className='w-full h-full object-scale-down lg:scale-125'
+                className='w-full h-full object-scale-down lg:scale-125 rounded-t-xl'
             />
       </div>
       <div className='flex items-center gap-1'>
@@ -40,7 +42,7 @@ const CardProduct = ({data}) => {
       <div className='px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base'>
         <div className='flex items-center gap-1'>
           <div className='font-semibold'>
-              {pricewithDiscount(data?.price,data?.discount)} 
+              {pricewithDiscount(data?.price,data?.discount)} $
           </div>
           
           
@@ -51,9 +53,9 @@ const CardProduct = ({data}) => {
               <p className='text-red-500 text-sm text-center'>Out of stock</p>
             ) : (
         //       <AddToCartButton data={data} />
-        <button>
-                add To Cart
-        </button>
+        <Button className='text-white' color="success" endContent={<IoCart className='text-white'/>}>
+        Add To Cart
+      </Button>
             )
           }
             

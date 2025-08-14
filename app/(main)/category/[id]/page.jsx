e5f74d0c47b04b30c19c3126d14933f1379d97fc
saman <sam.karimi727@gmail.com>
2025-08-15 02:08:01 +0330

    update page.jsx and CategorySidebar.jsx

--- app/(main)/category/[id]/page.jsx
+++ app/(main)/category/[id]/page.jsx
@@ -2,13 +2,16 @@
 import CategorySidebar from '@/components/category&subCategory/CategorySidebar'
 import React from 'react'
 
-const page = (props) => {
-        console.log(props)
+const page = ({params , searchParams}) => {
+        const {id} = params 
+        const {name} = searchParams
+        
   return (
         <section>
-        <CategorySidebar />
+        <CategorySidebar categoryId={id} name={name} />
   
-
+        <h1>{id}</h1>
+        <h1>{name}</h1>
 
     </section>
   )
--- components/category&subCategory/CategorySidebar.jsx
+++ components/category&subCategory/CategorySidebar.jsx
@@ -1,10 +1,43 @@
 
+import AxiosToastError from '@/utils/axiosToastError';
+import axios from 'axios'
 import React from 'react'
+import Loading from '../shared/loading';
+import Image from 'next/image';
 
-const CategorySidebar = () => {
+const CategorySidebar = async ({categoryId}) => {
+let loading = false
+let subCategoryData ;
+        try {
+                loading = true
+                const {data} = await axios.post('https://shopysam-back.onrender.com/api/subcategory/by-category-id',{
+                  categoryId: categoryId
+                })
+              
+          
+                if (data?.success) {
+                  subCategoryData= data?.data
+                }
+              } catch (error) {
+                AxiosToastError(error)
+              } finally {
+                loading = false
+              }
   return (
     <div className='w-[300px] h-screen fixed left-0 top-0 bg-red-400'>
-
+        <div className='flex flex-col gap-4 w-full'>
+                {
+                        loading && <Loading />
+                }
+                {
+                        subCategoryData?.map((c,index) => (
+                                <div key={index} className='w-full flex items-center gap-3'>
+                                        <Image src={c?.image} alt={c?.name} width={35} height={35} />
+                                        <h1>{c?.name}</h1>
+                                </div>
+                        ))
+                }
+        </div>
     </div>
   )
 }

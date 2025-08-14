7775525814e1be110073b68f45adfb39533ffa56
saman <sam.karimi727@gmail.com>
2025-08-15 01:45:41 +0330

    delete 3 files and create 1 file

---
+++ app/(main)/category/page.jsx
@@ -0,0 +1,16 @@
+
+import CategorySidebar from '@/components/category&subCategory/CategorySidebar'
+import React from 'react'
+
+const page = ({params}) => {
+        console.log(params)
+  return (
+    <section>
+        <CategorySidebar />
+        
+
+    </section>
+  )
+}
+
+export default page

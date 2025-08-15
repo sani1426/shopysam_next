8edf87f183859cd0b83e1f5f0299b8928b5e7df6
saman <sam.karimi727@gmail.com>
2025-08-15 12:26:32 +0330

    update CategoryWiseProductDisplay.jsx

--- components/main/CategoryWiseProductDisplay.jsx
+++ components/main/CategoryWiseProductDisplay.jsx
@@ -74,7 +74,7 @@ const CategoryWiseProductDisplay = ({ id, name }) => {
             <div>
                 <div className='container mx-auto p-4 flex items-center justify-between gap-4'>
                     <h3 className='font-semibold text-lg md:text-xl'>{name}</h3>
-                    <Link href='/'  className='text-green-600 hover:text-green-400'>See All</Link>
+                    <Link href={`/category/${id}?name=${name}`}  className='text-green-600 hover:text-green-400'>See All</Link>
                 </div>
                 <div className='relative flex items-center '>
                     <div className=' flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth py-4' ref={containerRef}>

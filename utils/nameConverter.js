a04ff0312f429aeac0992c5c32d589892c0bac0f
saman <sam.karimi727@gmail.com>
2025-08-17 00:53:36 +0330

    update nameConverter.js

--- utils/nameConverter.js
+++ utils/nameConverter.js
@@ -1,4 +1,4 @@
 export const valideURLConvert = (name)=>{
-        const url = name?.toString().replaceAll(",","-").replaceAll("&","-")
+        const url = name?.toString().replaceAll("&","-")
         return url
     }

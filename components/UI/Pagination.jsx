75afcdb78dc2bdd8c1880917d40c78cb4e6ca6e1
saman <sam.karimi727@gmail.com>
2025-08-18 00:13:24 +0330

    update Pagination.jsx

--- components/UI/Pagination.jsx
+++ components/UI/Pagination.jsx
@@ -1,5 +1,10 @@
+"use client"
+
+
 import {Pagination} from "@heroui/react";
+import { useEffect } from "react";
 
 export default function PaginationComponent({page , total , set}) {
+  useEffect(()=>{console.log(total)},[])
   return <Pagination showShadow color="warning" page={page} onChange={set} total={total} />;
 }

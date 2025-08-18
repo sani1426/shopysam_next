54e51d6713ae9fb4d4ea0f4a00d52ec804855c90
saman <sam.karimi727@gmail.com>
2025-08-18 03:32:10 +0330

    update Pagination.jsx

--- components/UI/Pagination.jsx
+++ components/UI/Pagination.jsx
@@ -18,9 +18,9 @@ export default function PaginationComponent({page , set , total}) {
       page={page}
         classNames={{
           wrapper: "gap-0 overflow-visible h-8 rounded-sm border border-divider",
-          item: "w-8 h-8 text-small rounded-none bg-transparent",
+          item: "w-8 h-8 text-small rounded-none bg-transparent cursor-pointer",
           cursor:
-            "bg-linear-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
+            "bg-linear-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold cursor-pointer",
         }}
         total={total}
       />
